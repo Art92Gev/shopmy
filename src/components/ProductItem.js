947b6cc3e4bc222f0b285b1f266676a03b46033d
Art92Gev <arturg995@gmail.com>
2024-10-11 13:50:05 +0400

    Update ProductItem.js

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,26 +1,29 @@
-
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles/ProductItem.css';
 
 function ProductItem({ product }) {
   const sendToTelegram = () => {
-    const chatId = "Artur0192121";
+    const chatId = "Artur0192121"; // Замените на ваш идентификатор Telegram.
     const productUrl = `https://${window.location.hostname}/product/${product.id}`;
     const message = `Название: ${product.name}\nОписание: ${product.description}\nЦена: ${product.price} руб.\n ||${product.images[0].thumbnail}|| \n ${productUrl}`;
     const telegramUrl = `tg://resolve?domain=${chatId}&text=${encodeURIComponent(message)}`;
     window.location.href = telegramUrl;
   };
 
-	const sendToWhatsApp = () => {
-		const phoneNumber = '37443996633'; // Замените на нужный номер телефона
-		const productUrl = `https://${window.location.hostname}/product/${product.id}`;
-		const message = `Название: ${product.name}\nОписание: ${product.description}\nЦена: ${product.price} руб.\n ${product.images[0].thumbnail} \n ${productUrl}`;
-	
-		const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-		window.location.href = whatsappUrl;
-	};
+  const sendToWhatsApp = () => {
+    const phoneNumber = '37443996633'; // Замените на нужный номер телефона.
+    const message = `Название: ${product.name}\nОписание: ${product.description}\nЦена: ${product.price} руб.\n${product.images[0].thumbnail}`;
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.location.href = whatsappUrl;
+  };
+
+  const sendToViber = () => {
+    const phoneNumber = '37443996633'; // Замените на нужный номер телефона.
+    const message = `Название: ${product.name}\nОписание: ${product.description}\nЦена: ${product.price} руб.\n${product.images[0].thumbnail}`;
+    const viberUrl = `viber://chat?number=%2B${phoneNumber}&text=${encodeURIComponent(message)}`;
+    window.location.href = viberUrl;
+  };
 
   return (
     <div className="product-item">
@@ -33,6 +36,7 @@ function ProductItem({ product }) {
       <p>{product.price} руб.</p>
       <button onClick={sendToTelegram}>Отправить в Telegram</button>
       <button onClick={sendToWhatsApp}>Отправить в WhatsApp</button>
+      <button onClick={sendToViber}>Отправить в Viber</button>
     </div>
   );
 }
