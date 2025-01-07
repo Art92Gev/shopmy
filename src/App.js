f6384fad79e22d1533228aa781d384882421b2dd
Art92Gev <arturg995@gmail.com>
2025-01-07 16:46:03 +0400

    t5t

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import CategoryList from './components/CategoryList';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
 
 function App() {
+  const navigate = useNavigate(); // Инициализация useNavigate
+
   return (
     <div className="App">
-			<div className='main-menu'>
-			</div>
-      <h1>Магазин</h1>
+      <div className="main-menu">
+        {/* Исправлено onClick */}
+        <img
+          onClick={() => navigate("/")} 
+          src="https://yerefan.ucoz.net/teana/dall-e_2025-01-07_16.20.28-a_modern_and_sleek_hori.webp"
+          alt="Car Accessories Logo"
+          className="logo"
+        />
+      </div>
+      <h1 onClick={() => navigate('/')}></h1> {/* Передача функции в onClick */}
       <Routes>
         <Route path="/" element={<CategoryList />} />
         <Route path="/category/:categoryName" element={<ProductList />} />
         <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
+      <div className="fill"></div>
     </div>
   );
 }
--- src/components/data/products.js
+++ src/components/data/products.js
@@ -11,29 +11,134 @@ const products = [
       // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
     ]
   },
-  {
+	{
     id: 2,
-		qty: '4',
     category: "Nissan Teana J31",
-    name: "Запчасть 2",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
     description: "",
-    price: "2000",
+    price: "8000",
     images: [
-      { original: "https://yerefan.ucoz.net/teana/processed_image_1.jpg", thumbnail: "https://yerefan.ucoz.net/teana/processed_image_1.jpg" },
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+    ]
+  },  {
+    id: 3,
+    category: "Nissan Teana J31",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
+    description: "",
+    price: "8000",
+    images: [
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
     ]
   },
-  {
-    id: 3,
-		qty: '4',
+	{
+    id: 4,
     category: "Nissan Teana J31",
-    name: "Փոխանցման տուփի բռնակ",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
     description: "",
-    price: "3000",
+    price: "8000",
     images: [
-      { original: "https://yerefan.ucoz.net/teana/processed_image_1.jpg", thumbnail: "https://yerefan.ucoz.net/teana/processed_image_1.jpg" },
-      { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
     ]
-  }
+  },  {
+    id: 5,
+    category: "Nissan Teana J31",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
+    description: "",
+    price: "8000",
+    images: [
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+    ]
+  },
+	{
+    id: 6,
+    category: "Nissan Teana J31",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
+    description: "",
+    price: "8000",
+    images: [
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+    ]
+  },
+	{
+    id: 1,
+    category: "Nissan Teana J31",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
+    description: "",
+    price: "8000",
+    images: [
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+    ]
+  },
+	{
+    id: 2,
+    category: "Nissan Teana J31",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
+    description: "",
+    price: "8000",
+    images: [
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+    ]
+  },  {
+    id: 3,
+    category: "Nissan Teana J31",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
+    description: "",
+    price: "8000",
+    images: [
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+    ]
+  },
+	{
+    id: 4,
+    category: "Nissan Teana J31",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
+    description: "",
+    price: "8000",
+    images: [
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+    ]
+  },  {
+    id: 5,
+    category: "Nissan Teana J31",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
+    description: "",
+    price: "8000",
+    images: [
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+    ]
+  },
+	{
+    id: 6,
+    category: "Nissan Teana J31",
+		qty: '3',
+    name: "Պատուհանների բլոկ",
+    description: "",
+    price: "8000",
+    images: [
+      { original: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg", thumbnail: "https://yerefan.ucoz.net/teana/photo_1_2024-12-20_12-23-528.jpg" },
+      // { original: "https://via.placeholder.com/600", thumbnail: "https://via.placeholder.com/150" }
+    ]
+  },
 ];
 
 export const categories = [
